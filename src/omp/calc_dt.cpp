5085777b830264f2f00c4bb5514a74da9f3fbcfb
thiao <thiapenda2503@gmail.com>
2026-02-28 23:06:57 +0100

    update

--- src/omp/calc_dt.cpp
+++ src/omp/calc_dt.cpp
@@ -46,8 +46,11 @@ void calc_dt_kernel(int x_min, int x_max, int y_min, int y_max, double dtmin, do
   // XXX we can't reduce to a reference for NVHPC, see
   // https://forums.developer.nvidia.com/t/nvc-f-0000-internal-compiler-error-unhandled-size-for-preparing-max-constant/221740
   double dt_min_val0 = dt_min_val;
-#pragma omp parallel for simd collapse(2) reduction(min : dt_min_val0)
+//#pragma omp parallel for simd collapse(2) reduction(min : dt_min_val0)
+// Le collapse(2) réduis un peu  la vectorisation
+#pragma omp parallel for
 for (int j = (y_min + 1); j < (y_max + 2); j++) {
+    #pragma omp simd reduction(min:dt_min_val0)
     for (int i = (x_min + 1); i < (x_max + 2); i++) {
       //Précharger les données pour eviter beaucoup d'appels de fonction
       double vol = volume(i,j);
